eaa21f0396c59f3fed915f2e9c1c91d479fc47fa
xXoDoSs <alexander.hoss@bechtle.com>
2025-11-03 16:04:23 +0100

    Update app.module.ts

--- frontend/spa/src/app/app.module.ts
+++ frontend/spa/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app.routing.module';
 import { APOLLO_OPTIONS} from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client/core';
@@ -12,7 +11,6 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule
   ],
   providers: [
